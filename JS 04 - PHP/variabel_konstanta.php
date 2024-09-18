8f8e19d2d292dd94b9a0a5ac6aff3248c7d68b8d
apriliaputri04 <apriliaputri8057@gmail.com>
2024-09-18 21:49:34 +0700

    JS4 Pratikum1

--- JS 04 - PHP/variabel_konstanta.php
+++ JS 04 - PHP/variabel_konstanta.php
@@ -2,15 +2,15 @@
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
-echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.<br>";
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.<br>"; 
 
 $benar = true;
 $salah = false;
-echo "Variabel benar: " . ($benar ? "true" : "false") . ", Variabel salah: " . ($salah ? "true" : "false") . "<br>";
+echo "Variabel benar: $benar<br>"; 
+echo "Variabel salah: $salah<br>"; 
 
 define("NAMA_SITUS", "WebsiteKu.com");
 define("TAHUN_PENDIRIAN", 2023);
 
-echo "Selamat datang di " . NAMA_SITUS . ".<br>";
-echo "Situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . ".<br>";
+echo "Selamat datang di ". NAMA_SITUS . ", situs yang didirikan pada tahun ". TAHUN_PENDIRIAN . ".<br>"; 
 ?>

0f20127e7e9548c6252f9539c35fc714b60d9201
apriliaputri04 <apriliaputri8057@gmail.com>
2024-09-24 20:41:45 +0700

    JS5 Pratikum1

--- JS 05 - PHP-2/array_1.php
+++ JS 05 - PHP-2/array_1.php
@@ -3,14 +3,14 @@
 <head>
 </head>
 <body>
-<h2>Array Terindeks</h2>
+<h2>Array Terindeks Menggunakan Loop</h2>
 <p>
 <?php
-$Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+$Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
 
-echo $Listdosen[2] . "<br>";
-echo $Listdosen[0] . "<br>";
-echo $Listdosen[1] . "<br>";
+foreach ($Listdosen as $dosen) {
+    echo $dosen . "<br>";
+}
 ?>
 </p>
 </body>

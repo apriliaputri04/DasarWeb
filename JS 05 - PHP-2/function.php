d078f3d956e10a1d0246d6eb98201c47476c10b8
apriliaputri04 <apriliaputri8057@gmail.com>
2024-09-24 21:18:00 +0700

    JS5 Pratikum4

---
+++ JS 05 - PHP-2/function.php
@@ -0,0 +1,12 @@
+<?php
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Elok<br/>"; // Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+// Memanggil fungsi yang sudah dibuat dua kali
+perkenalan();
+echo "<br/>"; // Memberikan jarak antar output
+perkenalan();
+?>

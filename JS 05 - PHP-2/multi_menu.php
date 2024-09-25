a263e7dfa35a403ef60b522a332c9aca86e49201
apriliaputri04 <apriliaputri8057@gmail.com>
2024-09-25 11:11:01 +0700

    JS5 Pratikum6

--- JS 05 - PHP-2/multi_menu.php
+++ JS 05 - PHP-2/multi_menu.php
@@ -1,50 +1,45 @@
-<?
+<?php
 $menu = [
-    ["nama" => "Beranda"
-    ],
-    [
-        "nama" => "Berita",
+  [
+    "nama" => "Beranda"
+  ],
+  [
+    "nama" => "Berita",
+    "subMenu" => [
+      [
+        "nama" => "Wisata",
         "subMenu" => [
-            ["nama" => "Wisata",
-                "subMenu" => [
-                    [
-                        "nama" => "Pantai"
-                    ],
-                    [
-                        "nama" => "Gunung"
-                    ]
-                ]
-            ],
-            [
-                "nama" => "Kuliner"
-            ],
-            [
-                "nama" => "Hiburan"
-            ]
+          [
+            "nama" => "Pantai"
+          ],
+          [
+            "nama" => "Gunung"
+          ]
         ]
-    ],
-    [
-        "nama" => "Tentang"
-    ],
-    [
-        "nama" => "Kontak"
-    ] 
+      ],
+      [
+        "nama" => "Kuliner"
+      ],
+      [
+        "nama" => "Hiburan"
+      ]
+    ]
+  ],
+  [
+    "nama" => "Tentang"
+  ],
+  [
+    "nama" => "Kontak"
+  ],
 ];
-
-function tampilkanMenuBertingkat(array $menu, $level = 1) {
-    $tagList = ($level % 2 == 1) ? 'ul' : 'ol'; // Alternatif antara ul dan ol
-    echo "<$tagList>";
-
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
     foreach ($menu as $item) {
+        // Tampilkan nama item
         echo "<li>{$item['nama']}";
-        if (isset($item['subMenu'])) {
-            tampilkanMenuBertingkat($item['subMenu'], $level + 1);
-        }
-        echo "</li>";
+        
     }
-
-    echo "</$tagList>";
+    echo "</ul>";
 }
-
 tampilkanMenuBertingkat($menu);
 ?>

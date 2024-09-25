51c76b38e4dfb03cd9fd493020967003e05c2347
apriliaputri04 <apriliaputri8057@gmail.com>
2024-09-25 13:45:54 +0700

    JS5 Pratikum7

--- JS 05 - PHP-2/string1.php
+++ JS 05 - PHP-2/string1.php
@@ -1,5 +1,4 @@
 <?php
-
 $loremIpsum = "Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Voluptatem reprehenderit nobis veritatis commodi fugiat molestias
     impedit unde ipsum voluptatum, corrupti minus sit excepturi nostrum
@@ -8,5 +7,5 @@ echo "<p>{$loremIpsum}</p>";
 echo "Panjang karakter: " . strlen($loremIpsum) . "<br>";
 echo "Panjang kata: " . str_word_count($loremIpsum) . "<br>";
 echo "<p>". strtoupper($loremIpsum). "</p>";
-echo "<p?". strtolower($loremIpsum) . "</p>";
+echo "<p>". strtolower($loremIpsum) . "</p>";
 ?>

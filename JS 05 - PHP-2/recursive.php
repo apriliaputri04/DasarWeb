b38e3fda4faba8406c3a1e2e4c8d0e1bc96072ae
apriliaputri04 <apriliaputri8057@gmail.com>
2024-09-25 10:50:22 +0700

    JS5 Pratikum5

--- JS 05 - PHP-2/recursive.php
+++ JS 05 - PHP-2/recursive.php
@@ -1,18 +1,7 @@
 <?php
-function tampilkanAngka(int $jumlah, int $indeks = 1) {
-    echo "Perulangan ke-{$indeks} <br>";
-    
-    // Melakukan perulangan dari 1 hingga 25
-    for ($i = 1; $i <= 25; $i++) {
-        echo "Angka ke-{$i} <br>";
-    }
-    
-    // Memanggil diri sendiri selama $indeks < $jumlah
-    if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks + 1);   
-    }
+function tampilkanHaloDunia(){
+echo "Halo dunia! <br>";
+tampilkanHaloDunia();
 }
-
-// Memanggil fungsi dengan argumen 20
-tampilkanAngka(20); 
+tampilkanHaloDunia();
 ?>

24166cb7af1f240ff136e496b3175b73465fed5e
apriliaputri04 <apriliaputri8057@gmail.com>
2024-10-08 22:53:12 +0700

    JS07 Pratikum3 Update langkah3

--- JS 07 - PHP From Processing/proses_from.php
+++ JS 07 - PHP From Processing/proses_from.php
@@ -1,7 +1,7 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nama = $_POST["nama"];
-        $email + $_POST["email"];
+        $email = $_POST["email"];
 
         echo "<h3>Output:</h3>";
         echo "Nama: " . $nama . "<br>";

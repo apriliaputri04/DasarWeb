ac224f764b7579abe02d7d751a15dd81d37e56fa
apriliaputri04 <apriliaputri8057@gmail.com>
2024-10-08 23:09:10 +0700

    JS07 Pratikum4 langkah6

--- JS 07 - PHP From Processing/html_aman.php
+++ JS 07 - PHP From Processing/html_aman.php
@@ -8,6 +8,12 @@
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="input">Input:</label>
         <input type="text" name="input" id="input" required>
+        <br><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+        <br><br>
+
         <input type="submit" name="submit" value="Submit">
     </form>
 
@@ -21,6 +27,18 @@
         // Tampilkan hasil input yang sudah disanitasi
         echo "<h3>Input yang diterima:</h3>";
         echo $input;
+        
+        // Memeriksa apakah input adalah email yang valid
+        $email = $_POST['email'];
+        
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Lanjutkan dengan pengolahan email yang aman
+            echo "<h3>Email yang diterima:</h3>";
+            echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+        } else {
+            // Tangani input yang tidak valid
+            echo "<h3>Email tidak valid!</h3>";
+        }
     }
     ?>
 </body>

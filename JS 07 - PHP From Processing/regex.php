ccfba4b68c39d14ef57f53131c332d87ed67e86d
apriliaputri04 <apriliaputri8057@gmail.com>
2024-10-09 13:00:35 +0700

    JS07 Pratikum5 langkah 17

--- JS 07 - PHP From Processing/regex.php
+++ JS 07 - PHP From Processing/regex.php
@@ -26,9 +26,8 @@ $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; // Output: "I like banana pie."
 
 echo "<br>";
-$pattern = '/go*d/'; // Cocokkan "god", "good", "gooood", dll.
+$pattern = '/go?d/'; // Cocokkan "god", "good", "gooood", dll.
 $text = 'god is good.';
-
 if (preg_match($pattern, $text, $matches)) {
     echo "Cocokkan: " . $matches[0];
 } else {

749d9006a74487fa0533f4001220a0e57f49988a
apriliaputri04 <apriliaputri8057@gmail.com>
2024-10-09 12:15:18 +0700

    JS07 Pratikum2 langkah1

--- JS 07 - PHP From Processing/empty.php
+++ JS 07 - PHP From Processing/empty.php
@@ -1,6 +1,8 @@
-$myArray = array(); // Array kosong
+<?php
+$myArray = array();
 if (empty($myArray)) {
     echo "Array tidak terdefinisi atau kosong.";
 } else {
     echo "Array terdefinisi dan tidak kosong.";
 }
+?>

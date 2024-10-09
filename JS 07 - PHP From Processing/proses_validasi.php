7f6367d9580a7288c1001d801fb8520ae0be8e82
apriliaputri04 <apriliaputri8057@gmail.com>
2024-10-09 15:23:59 +0700

    JS07 Pratikum7 langkah2

--- JS 07 - PHP From Processing/proses_validasi.php
+++ JS 07 - PHP From Processing/proses_validasi.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Form Input dengan Validasi</title>
-</head>
-<body>
-    <h1>Form Input dengan Validasi</h1>
-    <form method="post" action="proses_validasi.php">
-        <label for="nama">Nama:</label>
-        <input type="text" id="nama" name="nama">
-
-        <br><br>
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email">
-
-        <br><br>
-        <input type="submit" value="Submit">
-    </form>
-
-    <?php
+<?php
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $nama = $_POST['nama'];
         $email = $_POST['email'];
@@ -47,6 +28,4 @@
             echo "Data berhasil dikirim:  Nama = " . $nama . ", Email = " . $email;
         }
     }
-    ?>
-</body>
-</html>
+?>

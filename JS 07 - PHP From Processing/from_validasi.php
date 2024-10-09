62c66856b580fda667c6ca2e9950e34a9dc51b8d
apriliaputri04 <apriliaputri8057@gmail.com>
2024-10-09 14:13:00 +0700

    JS07 Partikum7 langkah6

--- JS 07 - PHP From Processing/from_validasi.php
+++ JS 07 - PHP From Processing/from_validasi.php
@@ -2,51 +2,47 @@
 <html>
 <head>
     <title>Form Input dengan Validasi</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <h1>Form Input dengan Validasi</h1>
-    <form method="post" action="proses_validasi.php">
+    <form id="myForm" method="post" action="proses_validasi.php">
         <label for="nama">Nama:</label>
         <input type="text" id="nama" name="nama">
+        <span id="nama-error" style="color: red;"></span><br><br>
 
-        <br><br>
         <label for="email">Email:</label>
         <input type="text" id="email" name="email">
+        <span id="email-error" style="color: red;"></span><br><br>
 
-        <br><br>
         <input type="submit" value="Submit">
     </form>
+    <script>
+        $(document).ready(function() {
+            $("#myForm").submit(function(event) {
+                var nama = $("#nama").val();
+                var email = $("#email").val();
+                var valid = true;
 
-    <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $nama = $_POST['nama'];
-        $email = $_POST['email'];
+                if (nama === "") {
+                    $("#nama-error").text("Nama harus diisi.");
+                    valid = false;
+                } else {
+                    $("#nama-error").text("");
+                }
 
-        $errors = array();
+                if (email === "") {
+                    $("#email-error").text("Email harus diisi.");
+                    valid = false;
+                } else {
+                    $("#email-error").text("");
+                }
 
-        // Validasi Nama
-        if (empty($nama)) {
-            $errors[] = "Nama harus diisi.";
-        }
-
-        // Validasi Email
-        if (empty($email)) {
-            $errors[] = "Email harus diisi.";
-        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = "Format email tidak valid.";
-        }
-
-        // Jika ada kesalahan validasi
-        if (!empty($errors)) {
-            foreach ($errors as $error) {
-                echo $error . "<br>";
-            }
-        } else {
-            // Lanjutkan dengan pemrosesan data jika semua validasi berhasil
-            // Misalnya, menyimpan data ke database atau mengirim email
-            echo "Data berhasil dikirim: Nama = " . $nama . ", Email = " . $email;
-        }
-    }
-    ?>
+                if (!valid) {
+                    event.preventDefault(); // Menghentikan pengiriman form jika validasi gagal
+                }
+            });
+        });
+    </script>
 </body>
 </html>

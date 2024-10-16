5e524697ab22a9877945bae10c4febb2e4f314dd
apriliaputri04 <apriliaputri8057@gmail.com>
2024-10-16 12:13:18 +0700

    JS08 Pratikum2 langkah5

--- JS08-PHPFromUpload,Cookies,Session/proses_upload.php
+++ JS08-PHPFromUpload,Cookies,Session/proses_upload.php
@@ -1,11 +1,14 @@
 <?php
 // Lokasi penyimpanan file yang diunggah
-$targetDirectory = "documents/";
+$targetDirectory = "uploads/";
 
 // Periksa apakah direktori penyimpanan ada, jika tidak maka buat
 if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
+
+// Daftar ekstensi yang diizinkan
+$allowedExtensions = array("jpg", "jpeg", "png", "gif");
 
 if ($_FILES['files']['name'][0]) {
     $totalFiles = count($_FILES['files']['name']);
@@ -13,13 +16,24 @@ if ($_FILES['files']['name'][0]) {
     // Loop melalui semua file yang diunggah
     for ($i = 0; $i < $totalFiles; $i++) {
         $fileName = $_FILES['files']['name'][$i];
-        $targetFile = $targetDirectory . $fileName;
+        $fileTmpName = $_FILES['files']['tmp_name'][$i];
+        $fileSize = $_FILES['files']['size'][$i];
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+        $targetFile = $targetDirectory . basename($fileName);
 
-        // Pindahkan file yang diunggah ke direktori penyimpanan
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah.<br>";
+        // Validasi ekstensi file
+        if (in_array($fileType, $allowedExtensions)) {
+            // Pindahkan file yang diunggah ke direktori penyimpanan
+            if (move_uploaded_file($fileTmpName, $targetFile)) {
+                echo "File $fileName berhasil diunggah.<br>";
+
+                // Tampilkan gambar thumbnail dengan lebar 200px
+                echo "<img src='$targetFile' width='200' style='margin: 10px;' alt='Thumbnail'><br>";
+            } else {
+                echo "Gagal mengunggah file $fileName.<br>";
+            }
         } else {
-            echo "Gagal mengunggah file $fileName.<br>";
+            echo "File $fileName bukan gambar yang valid.<br>";
         }
     }
 } else {

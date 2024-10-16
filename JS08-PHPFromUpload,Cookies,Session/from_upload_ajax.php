d53871e6bdb350dfa39c689b712403df3b13a8b0
apriliaputri04 <apriliaputri8057@gmail.com>
2024-10-16 14:21:25 +0700

    P2 L5 Update

--- JS08-PHPFromUpload,Cookies,Session/from_upload_ajax.php
+++ JS08-PHPFromUpload,Cookies,Session/from_upload_ajax.php
@@ -1,17 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Unggah File Gambar</title>
+    <title>Multiupload Dokumen</title>
 </head>
 <body>
-    <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-        <!-- Izinkan multi-upload dan hanya gambar -->
-        <input type="file" name="file[]" id="file" accept="image/*" multiple>
-        <input type="submit" name="submit" value="Unggah">
+    <h2>Unggah Dokumen</h2>
+    <form action="proses_upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="files[]" multiple="multiple" accept=".pdf, .doc, .docx">
+        <input type="submit" value="Unggah" />
     </form>
-    <div id="status"></div>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="upload.js"></script>
 </body>
 </html>

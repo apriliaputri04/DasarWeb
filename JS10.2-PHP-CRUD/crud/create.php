e29f3b8780e08a3f56f3a3885d6d2b0db5cf1d27
apriliaputri04 <apriliaputri8057@gmail.com>
2024-11-03 16:39:58 +0700

    JS10.2 Update pratikum 1 sampai 5

--- JS10.2-PHP-CRUD/crud/create.php
+++ JS10.2-PHP-CRUD/crud/create.php
@@ -7,33 +7,39 @@
 <body>
     <div class="container mt-4">
         <h2>Tambah Data Anggota</h2>
-        <form action="proses.php?aksi=tambahkan" method="post">
+
+        <form action="proses.php?aksi=tambah" method="post">
             <div class="form-group">
                 <label for="nama">Nama:</label>
                 <input type="text" class="form-control" name="nama" id="nama" required>
             </div>
+
             <div class="form-group">
                 <label for="jenis_kelamin">Jenis Kelamin:</label>
                 <div class="form-check">
-                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="laki" id="laki" required>
+                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="L" id="laki" required>
                     <label class="form-check-label" for="laki">Laki-laki</label>
                 </div>
                 <div class="form-check">
-                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="perempuan" id="perempuan" required>
+                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="P" id="perempuan" required>
                     <label class="form-check-label" for="perempuan">Perempuan</label>
                 </div>
             </div>
+
             <div class="form-group">
                 <label for="alamat">Alamat:</label>
                 <input type="text" class="form-control" name="alamat" id="alamat" required>
             </div>
+
             <div class="form-group">
                 <label for="no_telp">No. Telp:</label>
                 <input type="text" class="form-control" name="no_telp" id="no_telp" required>
             </div>
+
             <button type="submit" class="btn btn-primary">Simpan Data</button>
-            <a class="btn btn-secondary mt-2" href="index.php">Kembali</a>
         </form>
+
+        <a class="btn btn-secondary mt-2" href="index.php">Kembali</a>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
--- JS10.2-PHP-CRUD/crud/proses.php
+++ JS10.2-PHP-CRUD/crud/proses.php
@@ -2,13 +2,14 @@
 include('koneksi.php');
 
 $aksi = $_GET['aksi'];
-$nama = $_POST['nama'] ?? null;
-$jenis_kelamin = $_POST['jenis_kelamin'] ?? null;
-$alamat = $_POST['alamat'] ?? null;
-$no_telp = $_POST['no_telp'] ?? null;
+$nama = $_POST['nama'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
+$alamat = $_POST['alamat'];
+$no_telp = $_POST['no_telp'];
 
 if ($aksi == 'tambah') {
     $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
+
     if (mysqli_query($koneksi, $query)) {
         header("Location: index.php");
         exit();
@@ -19,6 +20,7 @@ if ($aksi == 'tambah') {
     if (isset($_POST['id'])) {
         $id = $_POST['id'];
         $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id='$id'";
+
         if (mysqli_query($koneksi, $query)) {
             header("Location: index.php");
             exit();
@@ -31,24 +33,20 @@ if ($aksi == 'tambah') {
 } elseif ($aksi == 'hapus') {
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
-
-        $query = "DELETE FROM anggota WHERE id = $id";
+        $query = "DELETE FROM anggota WHERE id='$id'";
 
         if (mysqli_query($koneksi, $query)) {
             header("Location: index.php");
             exit();
         } else {
             echo "Gagal menghapus data: " . mysqli_error($koneksi);
-            exit();
         }
     } else {
         echo "ID tidak valid.";
     }
 } else {
-    header("Location: index.php");
+    header("Location: idex.php");
 }
 
 mysqli_close($koneksi);
-
-
-mysqli_close($koneksi);
+?>

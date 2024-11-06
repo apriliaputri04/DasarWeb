08340991e2893081b8ae76b9c344f061b51b48f3
apriliaputri04 <apriliaputri8057@gmail.com>
2024-11-06 15:13:04 +0700

    JS11 P4 L6 Update

--- JS11-PHP-WebDinamisStruktural/fungsi/hapus.php
+++ JS11-PHP-WebDinamisStruktural/fungsi/hapus.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-
-if (empty($_SESSION['username'])) {
+if (!empty($_SESSION['username'])) {
     require '../config/koneksi.php';
     require '../fungsi/pesan_kilat.php';
     require '../fungsi/anti_injection.php';
@@ -9,14 +8,25 @@ if (empty($_SESSION['username'])) {
     if (!empty($_GET['jabatan'])) {
         $id = antiinjection($koneksi, $_GET['id']);
         $query = "DELETE FROM jabatan WHERE id = '$id'";
-
+        
         if (mysqli_query($koneksi, $query)) {
             pesan('success', "Jabatan Telah Terhapus.");
         } else {
             pesan('danger', "Jabatan Tidak Terhapus Karena: " . mysqli_error($koneksi));
         }
-    }
 
-    header("Location: ../index.php?page=jabatan");
+        header("Location: ./index.php?page=jabatan");
+    } else if (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+        $query = "DELETE FROM user WHERE user_id = '$id'";
+        
+        if (mysqli_query($koneksi, $query)) {
+            pesan('success', "Anggota Telah Terhapus.");
+        } else {
+            pesan('danger', "Data Login Terhapus Tetapi Data Anggota Todak Terhapus Karena: " . mysqli_error($koneksi));
+        }
+
+        header("Location: ./index.php?page=anggota");
+    }
 }
 ?>

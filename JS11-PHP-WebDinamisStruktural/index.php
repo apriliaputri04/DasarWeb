3d58f65579e62f85b4b7b667a2a85671e6edb0e7
apriliaputri04 <apriliaputri8057@gmail.com>
2024-11-06 14:26:05 +0700

    JS11 P1L1

--- JS11-PHP-WebDinamisStruktural/index.php
+++ JS11-PHP-WebDinamisStruktural/index.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-if (empty($_SESSION['level'])) {
+if (!empty($_SESSION['level'])) {
     require 'config/koneksi.php';
     require 'fungsi/pesan_kilat.php';
 
@@ -17,4 +17,3 @@ if (empty($_SESSION['level'])) {
 } else {
     header("Location: login.php");
 }
-?>

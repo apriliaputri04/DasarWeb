1d122eaa82c5ab0569932e96851d477db63e5f70
apriliaputri04 <apriliaputri8057@gmail.com>
2024-11-13 13:33:23 +0700

    JS12 P2 L2 Update

--- JS12-PHP-OOP/CRUD/database.php
+++ JS12-PHP-OOP/CRUD/database.php
@@ -4,14 +4,12 @@ class Database {
     private $username = "root";
     private $password = "";
     private $database = "prakwebdb";
-
     public $conn;
 
-    public function __construct() {
+    public function __construct(){
         $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-
         if ($this->conn->connect_error) {
-            die("Connection failed : " . $this->conn->connect_error);
+            die("Connection failed: " . $this->conn->connect_error);
         }
     }
 }

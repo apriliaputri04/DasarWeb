474ead8da4bab901cc4d3a4822f41467f37d8832
apriliaputri04 <apriliaputri8057@gmail.com>
2024-11-13 13:35:00 +0700

    JS12 P2 L6

--- JS12-PHP-OOP/CRUD/index.php
+++ JS12-PHP-OOP/CRUD/index.php
@@ -1,16 +1,20 @@
 <?php
-require_once "Database.php";
+require_once 'Crud.php';
 
 $crud = new Crud();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $jabatan = $_POST['jabatan'];
     $keterangan = $_POST['keterangan'];
-
     $crud->create($jabatan, $keterangan);
 }
 
-$data = $crud->read();
+if (isset($_GET['action']) && $_GET['action'] === 'delete') {
+    $id = $_GET['id'];
+    $crud->delete($id);
+}
+
+$tampil = $crud->read();
 ?>
 
 <!DOCTYPE html>
@@ -36,14 +40,14 @@ $data = $crud->read();
                 </tr>
             </thead>
             <tbody>
-                <?php 
+                <?php
                 foreach ($tampil as $show) {
                     echo "<tr>";
                     echo "<td>" . $show['id'] . "</td>";
                     echo "<td>" . $show['jabatan'] . "</td>";
                     echo "<td>" . $show['keterangan'] . "</td>";
                     echo "<td>";
-                    echo "<a href='edit.php?id=" . $show['id'] . "' class='btn btn-primary btn-sm'>Edit</a>";
+                    echo "<a href='edit.php?id=" . $show['id'] . "' class='btn btn-primary btn-sm'>Edit</a> ";
                     echo "<a href='index.php?action=delete&id=" . $show['id'] . "' class='btn btn-danger btn-sm'>Delete</a>";
                     echo "</td>";
                     echo "</tr>";
@@ -52,6 +56,7 @@ $data = $crud->read();
             </tbody>
         </table>
     </div>
+
     <div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -64,12 +69,12 @@ $data = $crud->read();
                 <div class="modal-body">
                     <form method="post" action="">
                         <div class="form-group">
-                            <label for="jabatan">Jabatan:</label>
+                            <label for="name">Jabatan:</label>
                             <input type="text" class="form-control" id="jabatan" name="jabatan" required>
                         </div>
                         <div class="form-group">
-                            <label for="keterangan">Keterangan:</label>
-                            <textarea name="keterangan" class="form-control" id="keterangan" cols="30" rows="3" required></textarea>
+                            <label for="email">Keterangan:</label>
+                            <textarea name="keterangan" class="form-control" id="keterangan" cols="30" rows="10" required></textarea>
                         </div>
                         <button type="submit" class="btn btn-primary">Tambah</button>
                     </form>
@@ -81,4 +86,5 @@ $data = $crud->read();
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
+
 </html>

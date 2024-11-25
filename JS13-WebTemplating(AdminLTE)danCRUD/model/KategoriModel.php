b0e85e724528fff8e484618489e060e69d2920e5
apriliaputri04 <apriliaputri8057@gmail.com>
2024-11-25 15:14:15 +0700

    JS13 tambahan P3 L3

--- JS13-WebTemplating(AdminLTE)danCRUD/model/KategoriModel.php
+++ JS13-WebTemplating(AdminLTE)danCRUD/model/KategoriModel.php
@@ -1,55 +1,87 @@
-<?php
-include('Model.php');
-class KategoriModel extends Model{
-private $db;
-private $table = 'm_kategori';
-public function __construct(){
-include_once('../lib/Connection.php');
-$this->db = $db;
-$this->db->set_charset('utf8');
+<?php  
+include('Model.php'); 
+ 
+class KategoriModel extends Model { 
+    protected $db; 
+    protected $table = 'm_kategori'; 
+    protected $driver; 
+ 
+    public function __construct() { 
+        include_once('../lib/Connection.php'); 
+        $this->db = $db; 
+        $this->driver = $use_driver; 
+    } 
+ 
+    public function insertData($data) { 
+        if ($this->driver == 'mysql') { 
+            // Prepare statement untuk query insert 
+            $query = $this->db->prepare("INSERT INTO {$this->table} (kategori_kode, kategori_nama) VALUES (?, ?)");
+            $query->bind_param('ss', $data['kategori_kode'], $data['kategori_nama']); 
+            $query->execute(); 
+        } else { 
+            // Query untuk menyimpan data ke database (SQL Server)
+            sqlsrv_query($this->db, "INSERT INTO {$this->table} (kategori_kode, kategori_nama) VALUES (?, ?)", [
+                $data['kategori_kode'], 
+                $data['kategori_nama']
+            ]); 
+        } 
+    } 
+ 
+    public function getData() { 
+        if ($this->driver == 'mysql') { 
+            // Query untuk mengambil semua data dari tabel 
+            return $this->db->query("SELECT * FROM {$this->table}")->fetch_all(MYSQLI_ASSOC); 
+        } else { 
+            // Query untuk mengambil semua data dari tabel (SQL Server)
+            $query = sqlsrv_query($this->db, "SELECT * FROM {$this->table}"); 
+            $data = []; 
+            while ($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) { 
+                $data[] = $row; 
+            } 
+            return $data; 
+        } 
+    } 
+ 
+    public function getDataById($id) { 
+        if ($this->driver == 'mysql') { 
+            // Query untuk mengambil data berdasarkan ID 
+            $query = $this->db->prepare("SELECT * FROM {$this->table} WHERE kategori_id = ?");
+            $query->bind_param('i', $id); 
+            $query->execute(); 
+            return $query->get_result()->fetch_assoc(); 
+        } else { 
+            // Query untuk mengambil data berdasarkan ID (SQL Server)
+            $query = sqlsrv_query($this->db, "SELECT * FROM {$this->table} WHERE kategori_id = ?", [$id]); 
+            return sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC); 
+        } 
+    } 
+ 
+    public function updateData($id, $data) { 
+        if ($this->driver == 'mysql') { 
+            // Query untuk mengupdate data berdasarkan ID 
+            $query = $this->db->prepare("UPDATE {$this->table} SET kategori_kode = ?, kategori_nama = ? WHERE kategori_id = ?");
+            $query->bind_param('ssi', $data['kategori_kode'], $data['kategori_nama'], $id); 
+            $query->execute(); 
+        } else { 
+            // Query untuk mengupdate data berdasarkan ID (SQL Server)
+            sqlsrv_query($this->db, "UPDATE {$this->table} SET kategori_kode = ?, kategori_nama = ? WHERE kategori_id = ?", [
+                $data['kategori_kode'], 
+                $data['kategori_nama'], 
+                $id
+            ]); 
+        } 
+    } 
+ 
+    public function deleteData($id) { 
+        if ($this->driver == 'mysql') { 
+            // Query untuk menghapus data berdasarkan ID 
+            $query = $this->db->prepare("DELETE FROM {$this->table} WHERE kategori_id = ?");
+            $query->bind_param('i', $id); 
+            $query->execute(); 
+        } else { 
+            // Query untuk menghapus data berdasarkan ID (SQL Server)
+            sqlsrv_query($this->db, "DELETE FROM {$this->table} WHERE kategori_id = ?", [$id]); 
+        } 
+    } 
 }
-public function insertData($data){
-// prepare statement untuk query insert
-$query = $this->db->prepare("insert into {$this->table} (kategori_kode,
-kategori_nama) values(?,?)");
-// binding parameter ke query, "s" berarti string, "ss" berarti dua string
-$query->bind_param('ss', $data['kategori_kode'], $data['kategori_nama']);
-// eksekusi query untuk menyimpan ke database
-$query->execute();
-}
-    public function getData()
-    {
-        // query untuk mengambil data dari tabel bank_soal
-        return $this->db->query("select * from {$this->table} ");
-    }
-    public function getDataById($id)
-    {
-        // query untuk mengambil data berdasarkan id
-        $query = $this->db->prepare("select * from {$this->table} where kategori_id = ?");
-        // binding parameter ke query "i" berarti integer. Biar tidak kena SQL Injection
-        $query->bind_param('i', $id);
-        // eksekusi query
-        $query->execute();
-        // ambil hasil query
-        return $query->get_result()->fetch_assoc();
-    }
-    public function updateData($id, $data)
-    {
-        // query untuk update data
-        $query = $this->db->prepare("update {$this->table} set kategori_kode = ?,
-        kategori_nama = ? where kategori_id = ?");
-        // binding parameter ke query
-        $query->bind_param('ssi', $data['kategori_kode'], $data['kategori_nama'], $id);
-        // eksekusi query
-        $query->execute();
-    }
-    public function deleteData($id)
-    {
-        // query untuk delete data
-        $query = $this->db->prepare("delete from {$this->table} where kategori_id = ?");
-        // binding parameter ke query
-        $query->bind_param('i', $id);
-        // eksekusi query
-        $query->execute();
-    }
-}
+?>
